2a9aa2abbcb353983328d0708970ea0561f41fea
정민석 <ms.jung.dev@gmail.com>
2026-03-15 23:05:26 +0900

    fix: CORS 필터 preflight/response 클래스 분리

--- src/main/kotlin/org/coffeezip/common/CorsFilter.kt
+++ src/main/kotlin/org/coffeezip/common/CorsFilter.kt
@@ -8,33 +8,32 @@ import jakarta.ws.rs.container.PreMatching
 import jakarta.ws.rs.core.Response
 import jakarta.ws.rs.ext.Provider
 
+private val ALLOWED_ORIGINS = setOf("http://localhost:3000")
+
 @Provider
 @PreMatching
-class CorsFilter : ContainerRequestFilter, ContainerResponseFilter {
-
-    private val allowedOrigins = setOf(
-        "http://localhost:3000"
-    )
-
+class CorsPreflightFilter : ContainerRequestFilter {
     override fun filter(requestContext: ContainerRequestContext) {
         val origin = requestContext.getHeaderString("Origin") ?: return
+        if (!requestContext.method.equals("OPTIONS", ignoreCase = true)) return
 
-        if (requestContext.method.equals("OPTIONS", ignoreCase = true)) {
-            requestContext.abortWith(
-                Response.ok()
-                    .header("Access-Control-Allow-Origin", if (origin in allowedOrigins) origin else "")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-                    .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-                    .header("Access-Control-Max-Age", "86400")
-                    .build()
-            )
-        }
+        requestContext.abortWith(
+            Response.ok()
+                .header("Access-Control-Allow-Origin", if (origin in ALLOWED_ORIGINS) origin else "")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+                .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+                .header("Access-Control-Max-Age", "86400")
+                .build()
+        )
     }
+}
 
+@Provider
+class CorsResponseFilter : ContainerResponseFilter {
     override fun filter(requestContext: ContainerRequestContext, responseContext: ContainerResponseContext) {
         val origin = requestContext.getHeaderString("Origin") ?: return
-        if (origin !in allowedOrigins) return
+        if (origin !in ALLOWED_ORIGINS) return
 
         responseContext.headers.putSingle("Access-Control-Allow-Origin", origin)
         responseContext.headers.putSingle("Access-Control-Allow-Credentials", "true")
